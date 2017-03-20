cc30acdba3102c3d5e907f7ae3634e99a7bd8871
Owen Mundy <omundy@users.noreply.github.com>
2017-03-20 09:10:52 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "removeGeojsonProps",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Owen Mundy",
+  "license": "MIT",
+  "dependencies": {
+    "fs": "0.0.1-security"
+  }
+}
